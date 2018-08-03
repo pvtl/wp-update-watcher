7c9569aec7a1be45374f55e0323ed870e54863f0
Matt Mcnamee <matt@mcnamee.co>
2018-08-03 10:51:12 +1000

    Update email notification

--- email/notification.php
+++ email/notification.php
@@ -8,9 +8,21 @@
 </head>
 <body>
 
-<p>Hi there, we've completed a scan of your website and determined that the following items are out of date and possibly insecure/vulnerable.</p>
-<p><?php echo nl2br($message); ?></p>
-<p>It's important to keep your website up to date to ensure security vulnerabilities are patched. Please contact your Pivotal Agency account manager (or simply reply to this email) and we'll provide a quote on updating these items and securing the site.</p>
+<p>Hi there,<br />
+We've completed a routine scan of your website and determined that the following items are out of date and possibly insecure/vulnerable.</p>
 
+<hr style="margin: 20px 0; border: 0; border-bottom: 1px dotted #eee;" />
+
+<p><?php echo nl2br($message); ?></p>
+
+<hr style="margin: 20px 0; border: 0; border-bottom: 1px dotted #eee;" />
+
+<p>It's important to keep your website up to date to ensure security vulnerabilities are patched.</p>
+<p>Please contact your Pivotal Agency account manager (or simply reply to this email) and we'll provide a quote on updating these items and securing the site.</p>
+
+<p>Kind regards,<br />
+<a href="https://pivotalagency.com.au" style="color: #F9C503;">Pivotal Agency</a></p>
+
+<p><img src="https://www.pivotalagency.com.au/email-signature/pivotal-agency.gif" style="width: 130px; height: auto;" /></p>
 </body>
 </html>
